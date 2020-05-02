ac3d6b7f8506c3118aaf1cb21b4b52f3f1035266
kerrmat <46334213+kerrmat@users.noreply.github.com>
2020-05-01 21:41:31 -0700

    Update stripMine.lua

--- stripMine.lua
+++ stripMine.lua
@@ -1,1 +1,20 @@
 
+turtle.dig()
+turtle.forward()
+turtle.turnRight()
+
+turtle.refuel()
+
+for i = 1,10,1
+do
+  for i = 1,10,1
+  do
+    turtle.dig()
+    turtle.forward()
+  end
+  turtle.digUp()
+  turtle.turnRight()
+  turtle.turnRight()
+end
+
+ 
