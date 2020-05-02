73e20574a766ed7184ceec0052e3677b26a5ba4a
kerrmat <46334213+kerrmat@users.noreply.github.com>
2020-05-02 16:07:52 -0700

    Update stripMine.lua

--- stripMine.lua
+++ stripMine.lua
@@ -4,16 +4,20 @@ turtle.refuel()
 print("Fuel Level is: ")
 print(turtle.getFuelLevel())
 
+-- length of tunnel, width and height are 10
 local length = 10
 
---width loop
+-- Relative position is marked by row primarily, then stack/height, then block
+local row = 0
+
+--width/row loop
 for i = 1,10,1
 do
-  --height loop
-  for i = 1,10,1
+  --height/stack loop
+  for j = 1,10,1
   do
-    --length loop
-    for i = 1,length,1
+    --length/block loop
+    for k = 1,length,1
     do
       turtle.dig()
       turtle.forward()
@@ -25,16 +29,55 @@ do
     turtle.turnRight()
   end
   --go down 11, right one, and turn
-  for i = 1,11,1
+  for j = 1,11,1
   do
     turtle.down()
   end
   
+  
+  
   --should eventually go back to chest to drop off and refuel here
-  turtle.turnRight()
-  turtle.dig()
-  turtle.forward()
+  
+  --go back to chest to drop off
   turtle.turnLeft()
+  for j = 0,row,1
+  do
+    turtle.forward()
+  end
+  turtle.turnLeft()
+  --drop
+  
+  turtle.turnLeft()
+  turtle.forward()
+  turtle.forward()
+  turtle.turnRight()
+  --get coal, put in slot 1
+  turtle.refuel()
+  
+  if row = 0
+  then
+    turtle.turnRight()
+    turtle.forward()
+    turtle.turnRight()
+  else 
+    if row = 1
+    then
+      turtle.turnRight()
+      turtle.turnRight()
+    else
+      turtle.turnLeft()
+      for j = 1,row,1
+      do
+        turtle.dig()
+        turtle.forward()
+      end
+      turtle.turnLeft()
+    end
+  end
+  
+  row = row + 1
+  
+  
 end
   
 
