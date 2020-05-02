56ebb849144b2b3909a05183b0d7713fd53986c2
kerrmat <46334213+kerrmat@users.noreply.github.com>
2020-05-01 22:02:55 -0700

    Update stripMine.lua

--- stripMine.lua
+++ stripMine.lua
@@ -4,17 +4,36 @@ turtle.refuel()
 print("Fuel Level is: ")
 print(turtle.getFuelLevel())
 
+local length = 10
+
+--width loop
 for i = 1,10,1
 do
+  --height loop
   for i = 1,10,1
   do
-    turtle.dig()
-    turtle.forward()
+    --length loop
+    for i = 1,length,1
+    do
+      turtle.dig()
+      turtle.forward()
+    end
+    --go up one and turn around
+    turtle.digUp()
+    turtle.up()
+    turtle.turnRight()
+    turtle.turnRight()
   end
-  turtle.digUp()
-  turtle.up()
-  turtle.turnRight()
-  turtle.turnRight()
+  --go down 10, right one, and turn
+  for i = 1,10,1
+  do
+    turtle.down()
+  end
+  turtle.turnLeft()
+  turtle.dig()
+  turtle.forward()
+  turtle.turnLeft()
 end
+  
 
  
